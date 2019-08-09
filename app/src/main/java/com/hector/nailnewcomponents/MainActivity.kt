c3e04c0634425b2aadfb3bb900a4a42516a6112b
Héctor López <hector.lopez@dsa-dev2-10>
2019-08-09 10:44:17 -0500

    import external module and modified strings

--- app/src/main/java/com/hector/nailnewcomponents/MainActivity.kt
+++ app/src/main/java/com/hector/nailnewcomponents/MainActivity.kt
@@ -2,11 +2,15 @@ package com.hector.nailnewcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.hector.mylibrary.ToolbarActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbarToLoad(toolbar as Toolbar)
     }
 }
